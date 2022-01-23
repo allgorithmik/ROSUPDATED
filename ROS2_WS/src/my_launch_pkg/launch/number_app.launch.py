c64b760d6593018244ffba3343b003c1206ae20f
allgorithmik <vvishwas2@gmail.com>
2022-01-23 19:45:42 +0530

    Added Launch file with parameters and configuration for Python/CPP nodes

--- ROS2_WS/src/my_launch_pkg/launch/number_app.launch.py
+++ ROS2_WS/src/my_launch_pkg/launch/number_app.launch.py
@@ -4,14 +4,28 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
+    remappings_ = ("publisher_node", "publisher_new")
+
     publisher_node = Node(
-        package = "my_python_pkg",
-        executable = "publisher"
+        package = "my_cpp_package",
+        executable = "number_publisher",
+        name = "renamed_publisher",
+        remappings = [
+            remappings_
+        ],
+        parameters = [
+            {"number_to_publish" : 5},
+            {"publish_frequency" : 10.0}
+        ]
     )
 
     subscriber_node = Node(
         package = "my_cpp_package",
-        executable = "subscriber"
+        executable = "subscriber",
+        name = "renamed_subscriber",
+        remappings = [
+            remappings_
+        ]
     )
     ld.add_action(publisher_node)
     ld.add_action(subscriber_node)
